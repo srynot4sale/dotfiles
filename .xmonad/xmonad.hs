cce295ba13c78c8fde38331962e50b858a1a3dfc
Aaron Barnes <aaron.barnes@hbcosmo.com>
2011-11-07 14:21:48 +1300

    .xmonad/: Switch to urxvt

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -28,7 +28,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal      = "gnome-terminal"
+myTerminal      = "urxvt"
  
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
