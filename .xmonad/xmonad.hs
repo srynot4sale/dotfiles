974f0fda079c570814c247f897fd9ae383dd1a26
Aaron Barnes <aaron.barnes@hbcosmo.com>
2012-05-24 11:44:34 +1200

    Add 'yeganesh' to improve dmenu results

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -93,7 +93,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
  
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    , ((modm,               xK_p     ), spawn "exe=`/home/aaronb/.cabal/bin/yeganesh` && eval \"exec $exe\"")
  
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
