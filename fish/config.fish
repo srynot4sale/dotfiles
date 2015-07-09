da9dacf62894ef889101f4ead1af9715856bb128
Aaron Barnes <aaron@io.net.nz>
2015-07-09 23:41:38 +1200

    Added oh-my-fish, and tidied fish config setup

--- fish/config.fish
+++ fish/config.fish
@@ -1,45 +1,13 @@
-alias j "/home/aaronb/code/personal/jarvis/bin/python /home/aaronb/code/personal/jarvis/clients/cli.py"
-alias l "ls --color=auto -alv"
-alias rgrep "grep --color=auto -RniP"
-alias top "atop"
-alias mv "mv -i"
+# Path to your oh-my-fish.
+set fish_path $HOME/.config/fish/omf
 
-alias gcb '__git_ps1 "%s"'
-alias psqlg 'psql gcb'
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+set fish_custom $HOME/.config/fish/custom
 
-alias tks 'pushd ~/code/dev/tks; perl tks.pl ~/code/dev/tks/timesheets/(date "+%Y-%m")-catalyst.tks -f month'
-alias tksedit 'vim -O ~/code/dev/tks/timesheets/(date "+%Y-%m")-catalyst.tks ~/.tksrc'
-alias tksmake 'set -l FNAME ~/code/dev/tks/timesheets/(date "+%Y-%m")-catalyst.tks; tks -t month >> $FNAME; echo "Saved to $FNAME"; popd;'
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
 
-alias decrypt_password '/home/aaronb/code/personal/dotfiles/decrypt_password.sh'
-
-alias b '/bin/bash'
-
-function whoami_if_other
-    if test "$USER" != "aaronb"
-        echo $USER
-    end
-end
-
-function maxlength_pwd
-    if test (pwd | head -c 12) = "/home/aaronb"
-        set APWD (pwd | tail -c +13)
-        echo "~$APWD"
-    else
-        echo $PWD
-    end
-end
-
-function fish_prompt
-    set_color red
-    echo -n (hostname)
-    echo -n " "
-    set_color 00AFFF
-    echo -n (maxlength_pwd)
-    echo -n (whoami_if_other)
-    set_color green
-    echo  (__fish_git_prompt)
-    set_color 00AFFF
-    echo -n '> '
-    set_color normal
-end
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+Theme 'bobthefish'
+Plugin 'theme'
