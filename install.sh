b3a9ed3b342ae2feb67f0af92e4d91e148ad5938
Aaron Barnes <aaron@io.nz>
2019-08-08 17:35:09 +1200

    Don't install i3 everywhere

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ ln -s "$SOURCE/.xinitrc" ~/.xinitrc
 ln -s "$SOURCE/.Xmodmap" ~/.Xmodmap
 
 echo "Install/update apt packages..."
-sudo apt-get install -y curl i3 wget vim postgresql-common libpq-dev silversearcher-ag python-dev ack-grep vim-gtk3 shellcheck zsh jq fonts-powerline fzf tree
+sudo apt-get install -y curl wget vim postgresql-common libpq-dev silversearcher-ag python-dev ack-grep vim-gtk3 shellcheck zsh jq fonts-powerline fzf tree
 
 echo "Install pip"
 rm -f get-pip.py
